f0ee66a423b176900d329bdf0ab0772aaa4471f6
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-04-25 18:05:52 +0200

    themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Button } from "./components/Button"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary"></Button>
       <Button variant="secondary"></Button>
       <Button variant="danger"></Button>
       <Button variant="success"></Button>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,7 +17,9 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary}
+
+    /*${props => {
         return css`background: ${buttonVariants[props.variant]}`
-    }}
+    }}*/
 `;
