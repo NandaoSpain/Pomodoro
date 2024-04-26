2d3b8b4fa6c54f3d9827def5b41b1fbc3af28b26
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-04-26 17:29:59 +0200

    FormContainer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,29 @@
 import { Play } from 'phosphor-react'
+import { HomeContainer, FormContainer, ContdownContainer } from './styles'
 
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form action="">
-        <div>
+        <FormContainer>
           <label htmlFor="task">I will go work in</label>
           <input id="task" type="text" />
           <label htmlFor="">Durante</label>
           <input id="minutesAmount" type="number" />
           <span>minutes.</span>
-        </div>
-        <div>
+        </FormContainer>
+        <ContdownContainer>
           <span>0</span>
           <span>0</span>
           <span>:</span>
           <span>0</span>
           <span>0</span>
-        </div>
+        </ContdownContainer>
         <button type="submit">
           <Play size={24} />
           Start
         </button>
       </form>
-    </div>
+    </HomeContainer>
   )
 }
---
+++ src/pages/Home/styles.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
+
+export const ContdownContainer = styled.div`
+
+`
