61f333da4cf986540a7b01eba5305e52c2f2eda1
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-04-26 16:37:23 +0200

    Logo Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from './styles'
 
+import logo from '../../assets/Logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logo} alt="Logotipo" />
       <nav>
-        <a href="#">Timer</a>
-        <a href="#">History</a>
+        <a href="#">
+          <Timer size={24} />
+        </a>
+        <a href="#">
+          <Scroll size={24} />
+        </a>
       </nav>
     </HeaderContainer>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,7 +4,4 @@ export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 1rem 2rem;
-  background-color: #000;
-  color: #fff;
 `
