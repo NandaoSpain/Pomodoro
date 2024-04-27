4c6fce7d4952435294fb0ddc0cc134581cc32316
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-04-27 16:10:18 +0200

    status color

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Play } from 'phosphor-react'
 import {
   HomeContainer,
@@ -10,6 +11,8 @@ import {
 } from './styles'
 
 export function Home() {
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <form action="">
@@ -20,6 +23,8 @@ export function Home() {
             type="text"
             list="task-suggestions"
             placeholder="Named your Project"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
           />
 
           <datalist id="task-suggestions">
@@ -47,7 +52,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </ContdownContainer>
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Start
         </StartCountdownButton>
