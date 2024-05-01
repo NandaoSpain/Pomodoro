d017de70deca60ce90bd45b212a9e98ccc8ac30a
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-05-01 16:03:05 +0200

    update reducer

--- src/reducers/cycles.ts
+++ src/reducers/cycles.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer'
+
 export interface Cycle {
   id: string
   task: string
@@ -21,23 +23,23 @@ export enum ActionTypes {
 export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      })
+
+      if (currentCycleIndex < 0) {
+        return state
       }
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, inTerruptedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+      return produce(state, (draft) => {
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].inTerruptedDate = new Date()
+      })
+    }
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
       return {
         ...state,
